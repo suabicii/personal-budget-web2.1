24731c4bc34470ec1509e6bf3b258d1fbfb59e71
suabicii <michal.slabik3@wp.pl>
2020-09-09 14:11:14 +0200

    Added displaying balance from current month in general view

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
 use Core\View;
 
 /**
@@ -19,5 +20,20 @@ class Balance extends \Core\Controller
     public function indexAction()
     {
         View::renderTemplate('Balance/balance.html');
+    }
+
+    /**
+     * Wyświetl bilans w widoku ogólnym
+     * 
+     * @return void
+     */
+    public function generalAction()
+    {
+        $user = User::findByID($_SESSION['user_id']);
+
+        $_SESSION['summed_incomes'] = $user->getSummedIncomes('2020-09-01', date('Y-m-d'));
+        $_SESSION['summed_expenses'] = $user->getSummedExpenses('2020-09-01', date('Y-m-d'));
+
+        $this->redirect('/balance');
     }
 }
