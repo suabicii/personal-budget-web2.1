b09711c0953d09458ce43da276729fec2fb7d06a
suabicii <michal.slabik3@wp.pl>
2020-09-18 14:08:03 +0200

    Added sending activation emails function

--- App/Mail.php
+++ App/Mail.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
-use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 /**
  * Wysyłanie maili do aktywacji kont lub resetowania haseł
@@ -13,5 +15,31 @@ class Mail
 {
     public static function send($to, $subject, $text, $html)
     {
+        $mail = new PHPMailer(true); // Tworzenie instancji i przekazywanie „true” włącza wyjątki
+
+        try {
+            //Ustawienia serwera
+            $mail->isSMTP();                                            // Wyślij używając SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Ustaw serwer SMTP do przesyłania
+            $mail->SMTPAuth   = true;                                   // Włącz uwierzytelnianie SMTP
+            $mail->Username   = 'personal.budget.slabikovsky@gmail.com';                     // nazwa użytkownika SMTP
+            $mail->Password   = 'Ubuntu2020!';                               // SMTP hasło
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Włącz szyfrowanie SMTP; 
+            $mail->Port       = 465;                                    // Port TCP do połączenia
+
+            //Adresat
+            $mail->setFrom('no-reply@michael.slabikovsky.com', 'Michael Slabikovsky');
+            $mail->addAddress($to);     // Dodaj adresata
+
+            // Treść wiadomości
+            $mail->isHTML(true);                                  // Ustaw format na HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $html;
+            $mail->AltBody = $text; // Wersja tekstowa (bez HTML-a)
+
+            $mail->send();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
 }
