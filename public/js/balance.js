7976faa1f55dd45900f7be4b3a58aa016b844012
suabicii <michal.slabik3@wp.pl>
2020-11-12 20:16:45 +0100

    Returned pie charts in particular view

--- public/js/balance.js
+++ public/js/balance.js
@@ -6,26 +6,99 @@
   let sumOfIncomes = document.getElementById("incomes");
   let sumOfExpenses = document.getElementById("expenses");
 
-  // Kategorie przychodów
+  /** Zapisywanie do tablic przychodów i wydatków */
+
   let rowsWithIncomes = document.querySelectorAll(".income");
   let incomes = [];
-
-  for (let i = 0; i < rowsWithIncomes.length; i++) {
-    incomes.push({
-      name: rowsWithIncomes[i].children[1].textContent,
-      amount: parseFloat(rowsWithIncomes[i].children[2].textContent),
-    });
-  }
-
-  // Kategorie wydatków
   let rowsWithExpenses = document.querySelectorAll(".expense");
   let expenses = [];
 
-  for (let i = 0; i < rowsWithExpenses.length; i++) {
-    expenses.push({
-      name: rowsWithExpenses[i].children[1].textContent,
-      amount: parseFloat(rowsWithExpenses[i].children[2].textContent),
+  // Sumowanie przychodów/wydatków przy widoku szczegółowym
+  const sumIncomes = (incomes) => {
+    let sum = 0;
+    if (incomes != null) {
+      incomes.forEach((income) => {
+        sum += parseFloat(income.textContent);
+      });
+    }
+    return sum;
+  };
+
+  const sumExpenses = (expenses) => {
+    let sum = 0;
+    if (expenses != null) {
+      expenses.forEach((expense) => {
+        sum += parseFloat(expense.textContent);
+      });
+    }
+    return sum;
+  };
+
+  if (!particularView) {
+    for (let i = 0; i < rowsWithIncomes.length; i++) {
+      incomes.push({
+        name: rowsWithIncomes[i].children[1].textContent,
+        amount: parseFloat(rowsWithIncomes[i].children[2].textContent), // zamiana string
+        // na liczbę (float)
+      });
+    }
+
+    for (let i = 0; i < rowsWithExpenses.length; i++) {
+      expenses.push({
+        name: rowsWithExpenses[i].children[1].textContent,
+        amount: parseFloat(rowsWithExpenses[i].children[2].textContent),
+      });
+    }
+  } else {
+    incomes.push({
+      name: rowsWithIncomes[0].children[1].textContent,
+      amount: sumIncomes(
+        document.querySelectorAll(
+          "." + rowsWithIncomes[0].children[3].className
+        )
+      ),
     });
+
+    for (let i = 1; i <= rowsWithIncomes.length; i++) {
+      if (
+        rowsWithIncomes[i].children[1].textContent !=
+        rowsWithIncomes[i - 1].children[1].textContent
+      ) {
+        incomes.push({
+          name: rowsWithIncomes[i].children[1].textContent,
+          amount: sumIncomes(
+            document.querySelectorAll(
+              "." + rowsWithIncomes[i].children[3].className
+            )
+          ),
+        });
+      }
+    }
+
+    expenses.push({
+      name: rowsWithExpenses[0].children[1].textContent,
+      amount: sumExpenses(
+        document.querySelectorAll(
+          "." + rowsWithExpenses[0].children[4].className
+        )
+      ),
+    });
+
+    for (let i = 1; i <= rowsWithExpenses.length; i++) {
+      if (
+        rowsWithExpenses[i].children[1].textContent !=
+        rowsWithExpenses[i - 1].children[1].textContent
+      ) {
+        expenses.push({
+          name: rowsWithExpenses[i].children[1].textContent,
+          amount: sumExpenses(
+            document.querySelectorAll(
+              "." + rowsWithExpenses[i].children[4].className
+            )
+          ),
+        });
+      }
+    }
   }
 
   difference.textContent = (
@@ -63,59 +136,6 @@
     chartWidth = 350;
   }
 
-  // Sumowanie przychodów/wydatków przy widoku szczegółowym
-  const sumIncomes = (incomes) => {
-    let sum = 0;
-    if (incomes != null) {
-      incomes.forEach((income) => {
-        sum += parseFloat(income.textContent); // zamiana string na liczbę (float)
-      });
-    }
-    return sum;
-  };
-
-  const sumExpenses = (expenses) => {
-    let sum = 0;
-    if (expenses != null) {
-      expenses.forEach((expense) => {
-        sum += parseFloat(expense.textContent);
-      });
-    }
-    return sum;
-  };
-
-  // Zmienne przypisane do poszczególnych kategorii
-  let salary;
-  let interest;
-  let allegro;
-  let anotherIncomes;
-  if (!particularView) {
-    salary = document.querySelector("#Salary")
-      ? parseFloat(document.querySelector("#Salary").textContent)
-      : 0;
-    interest = document.querySelector("#Interest")
-      ? parseFloat(document.querySelector("#Interest").textContent)
-      : 0;
-    allegro = document.querySelector("#Allegro")
-      ? parseFloat(document.querySelector("#Allegro").textContent)
-      : 0;
-    anotherIncomes = document.querySelector("#Another-Incomes")
-      ? parseFloat(document.querySelector("#Another-Incomes").textContent)
-      : 0;
-  } else {
-    const salaries = document.querySelectorAll(".Salary");
-    salary = sumIncomes(salaries);
-
-    const interests = document.querySelectorAll(".Interest");
-    interest = sumIncomes(interests);
-
-    const allegroIncomes = document.querySelectorAll(".Allegro");
-    allegro = sumIncomes(allegroIncomes);
-
-    const anotherIncomesAll = document.querySelectorAll(".Another-Incomes");
-    anotherIncomes = sumIncomes(anotherIncomesAll);
-  }
-
   const drawChartOfIncomes = () => {
     let data = new google.visualization.DataTable();
     data.addColumn("string", "Kategoria");
@@ -134,132 +154,6 @@
     );
     chart.draw(data, options);
   };
-
-  // Zmienne przypisane do poszczególnych kategorii
-  let food;
-  let apartments;
-  let transport;
-  let telecommunication;
-  let health;
-  let clothes;
-  let hygiene;
-  let kids;
-  let recreation;
-  let trip;
-  let courses;
-  let books;
-  let savings;
-  let retirement;
-  let debts;
-  let gift;
-  let anotherExpenses;
-
-  if (!particularView) {
-    food = document.querySelector("#Food")
-      ? parseFloat(document.querySelector("#Food").textContent)
-      : 0;
-    apartments = document.querySelector("#Apartments")
-      ? parseFloat(document.querySelector("#Apartments").textContent)
-      : 0;
-    transport = document.querySelector("#Transport")
-      ? parseFloat(document.querySelector("#Transport").textContent)
-      : 0;
-    telecommunication = document.querySelector("#Telecommunication")
-      ? parseFloat(document.querySelector("#Telecommunication").textContent)
-      : 0;
-    health = document.querySelector("#Health")
-      ? parseFloat(document.querySelector("#Health").textContent)
-      : 0;
-    clothes = document.querySelector("#Clothes")
-      ? parseFloat(document.querySelector("#Clothes").textContent)
-      : 0;
-    hygiene = document.querySelector("#Hygiene")
-      ? parseFloat(document.querySelector("#Hygiene").textContent)
-      : 0;
-    kids = document.querySelector("#Kids")
-      ? parseFloat(document.querySelector("#Kids").textContent)
-      : 0;
-    recreation = document.querySelector("#Recreation")
-      ? parseFloat(document.querySelector("#Recreation").textContent)
-      : 0;
-    trip = document.querySelector("#Trip")
-      ? parseFloat(document.querySelector("#Trip").textContent)
-      : 0;
-    courses = document.querySelector("#Courses")
-      ? parseFloat(document.querySelector("#Courses").textContent)
-      : 0;
-    books = document.querySelector("#Books")
-      ? parseFloat(document.querySelector("#Books").textContent)
-      : 0;
-    savings = document.querySelector("#Savings")
-      ? parseFloat(document.querySelector("#Savings").textContent)
-      : 0;
-    retirement = document.querySelector("#For-Retirement")
-      ? parseFloat(document.querySelector("#For-Retirement").textContent)
-      : 0;
-    debts = document.querySelector("#Debt-Repayment")
-      ? parseFloat(document.querySelector("#Debt-Repayment").textContent)
-      : 0;
-    gift = document.querySelector("#Gift")
-      ? parseFloat(document.querySelector("#Gift").textContent)
-      : 0;
-    anotherExpenses = document.querySelector("#Another-Expenses")
-      ? parseFloat(document.querySelector("#Another-Expenses").textContent)
-      : 0;
-  } else {
-    const foodAll = document.querySelectorAll(".Food");
-    food = sumExpenses(foodAll);
-
-    const apartmentsAll = document.querySelectorAll(".Apartments");
-    apartments = sumExpenses(apartmentsAll);
-
-    const transportAll = document.querySelectorAll(".Transport");
-    transport = sumExpenses(transportAll);
-
-    const telecommunicationAll = document.querySelectorAll(
-      ".Telecommunication"
-    );
-    telecommunication = sumExpenses(telecommunicationAll);
-
-    const healthAll = document.querySelectorAll(".Health");
-    health = sumExpenses(healthAll);
-
-    const clothesAll = document.querySelectorAll(".Clothes");
-    clothes = sumExpenses(clothesAll);
-
-    const hygieneAll = document.querySelectorAll(".Hygiene");
-    hygiene = sumExpenses(hygieneAll);
-
-    const kidsAll = document.querySelectorAll(".Kids");
-    kids = sumExpenses(kidsAll);
-
-    const recreationAll = document.querySelectorAll(".Recreation");
-    recreation = sumExpenses(recreationAll);
-
-    const tripAll = document.querySelectorAll(".Trip");
-    trip = sumExpenses(tripAll);
-
-    const coursesAll = document.querySelectorAll(".Courses");
-    courses = sumExpenses(coursesAll);
-
-    const booksAll = document.querySelectorAll(".Books");
-    books = sumExpenses(booksAll);
-
-    const savingsAll = document.querySelectorAll(".Savings");
-    savings = sumExpenses(savingsAll);
-
-    const retirementAll = document.querySelectorAll(".For-Retirement");
-    retirement = sumExpenses(retirementAll);
-
-    const debtsAll = document.querySelectorAll(".Debt-Repayment");
-    debts = sumExpenses(debtsAll);
-
-    const giftAll = document.querySelectorAll(".Gift");
-    gift = sumExpenses(giftAll);
-
-    const anotherExpensesAll = document.querySelectorAll(".Another-Expenses");
-    anotherExpenses = sumExpenses(anotherExpensesAll);
-  }
 
   const drawChartOfExpenses = () => {
     let data = new google.visualization.DataTable();
